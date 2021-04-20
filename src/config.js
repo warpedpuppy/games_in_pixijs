80e30077eaf2693e6b433dfc874c8fb935aef13f
ted <ted@warpedpuppy.com>
2021-04-20 10:32:50 -0400

    changed config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT_NOT: 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://game-support.herokuapp.com/api',
+  API_ENDPOINT: 'https://young-fjord-11847.herokuapp.com/api',
   TOKEN_KEY: 'MAZE_BUILDER'
 }
