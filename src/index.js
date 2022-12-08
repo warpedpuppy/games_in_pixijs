3d6a759232d05f0011b367d3b41e61b98b6b3175
ted <ted@warpedpuppy.com>
2022-12-08 13:42:50 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App'
+import './index.css';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <h1>hello</h1>
   </BrowserRouter>,
   document.getElementById('root')
 )
